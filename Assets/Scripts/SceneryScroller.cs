935eae7df75a4b5e090fac2d61bf547af3936b1a
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2016-03-31 16:59:46 +0200

    Moving Ragnar on scenery with scrolling effect

---
+++ Assets/Scripts/SceneryScroller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneryScroller : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D otherElement){
+		print (otherElement.tag);
+
+		if (otherElement.tag == "Ground") {
+			Vector2 position = otherElement.transform.position;
+
+			position.x += 2*31.0f;
+
+			otherElement.transform.position = position;
+		}
+
+		if (otherElement.tag == "Sky") {
+			Vector2 position = otherElement.transform.position;
+
+			position.x += 2*61.0f;
+
+			otherElement.transform.position = position;
+		}
+	}
+}
