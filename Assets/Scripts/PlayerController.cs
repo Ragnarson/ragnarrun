de43a818af1cd7515572fffcefa94271ca0d0efe
Stanisław Tuszyński <stanislaw.tuszynski@shellycloud.com>
2016-03-31 15:50:37 +0200

    Adds some scripts for our game :P

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public int life;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (5, 5);
+		} else {
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (5, 0);
+		}
+	}
+}
