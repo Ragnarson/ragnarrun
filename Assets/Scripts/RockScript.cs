9c2538e8d1135ec318cddaa4f9e1061b984e14e0
Sebastian Wojtczak <wojtczaksebastian@gmail.com>
2016-04-01 00:09:03 +0200

    Rock sprite and prefab

---
+++ Assets/Scripts/RockScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GemScript : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D otherElement)
+	{
+		// logic that kills Ragnar
+	}
+}
