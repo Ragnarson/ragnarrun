2e40b5cfebfa3e57d758e57922550bbc5b2615e1
Stanisław Tuszyński <stanislaw.tuszynski@shellycloud.com>
2016-04-01 12:48:43 +0200

    Workaround for gems disapearing.

--- Assets/Scripts/GemScript.cs
+++ Assets/Scripts/GemScript.cs
@@ -6,7 +6,7 @@ public class GemScript : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D otherElement)
 	{
 		// logic that takes care of increasing score
-		Destroy(gameObject);
+		gameObject.GetComponent<Transform>().position = new Vector2(50,50);
 		GameController.current.Scored ();
 	}
 }
